5535547d0fc2fbdfa737f29d1a04695f286e4466
Harsh Yadav <harshyadav.hy20@gmail.com>
2024-03-14 23:42:58 +0530

    more add ons - easy section done with scratching effect

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
-let backButton = document.querySelector('back-button-2');
-let cover= document.querySelector('.cover');
-let canvas= document.querySelector('canvas');
+const createScratchCard=(canvasId)=>{
+  let canvas = document.querySelector(canvasId);
+let context = canvas.getContext("2d");
 
-backButton.addEventListener('click', () => {
-  window.history.back();
-});
+const init = () => {
+  context.fillStyle = "grey";
+   context.fillRect(0, 0,200,150);
+  }
 
-canvas.addEventListener('click', () => {
-  canvas.style.background = rgba(0, 0, 0, 0.5);
-})
+  const scratch = (x, y) => {
+    context.globalCompositeOperation = "destination-out";
+    context.beginPath();
+    context.arc(x, y, 15, 0, Math.PI * 2);
+    context.fill();    
+  }
+  let isDrawing = false;
+  canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true;
+      scratch(e.offsetX, e.offsetY);
+  });
+  canvas.addEventListener("mousemove", (e) => {
+    if (isDrawing) {
+      scratch(e.offsetX, e.offsetY);
+    }
+  });
+  canvas.addEventListener("mouseup", () => {
+    isDrawing = false;
+  });
+  canvas.addEventListener("mouseleave", () => {
+    isDrawing = false;
+  });
+init();
 
+}
+for (let i = 1; i <= 1000; i++) {
+  if((i>=1&&i<=70) || i>=501 && i<=523 || i>=1001 && i<=1008)
+  createScratchCard(`.scratching${i}`);
+} 
+// for (let j = 501; j <= 523; j++) {
+//   createScratchCard(`.scratching${j}`);
+// } 
+// for (let k = 1001; k <= 1008; k++) {
+//   createScratchCard(`.scratching${k}`);
+// } 
