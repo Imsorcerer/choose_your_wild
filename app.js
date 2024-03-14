eb5767bc2e0b69386de0af6a4479a2f6aaffff7d
Harsh Yadav <harshyadav.hy20@gmail.com>
2024-03-14 14:34:01 +0530

    added cover and feature

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 let backButton = document.querySelector('back-button-2');
+let cover= document.querySelector('.cover');
+let canvas= document.querySelector('canvas');
 
 backButton.addEventListener('click', () => {
   window.history.back();
 });
+
+canvas.addEventListener('click', () => {
+  canvas.style.background = rgba(0, 0, 0, 0.5);
+})
+
